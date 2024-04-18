50f0ca5f9fd74ffe33a326ac28328b00af9547c2
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-04-18 16:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function stringChop(str, size) {
   // your code here
+	if(!str){
+		return [];
+	}
+
+	const chunks = [];
+	for(let i = 0;i<size;i+=lenght){
+		chunks.push(str.slice(i,i+length));
+	}
+	
+	return chunks;
+
+	
 }
 
 // Do not change the code below
